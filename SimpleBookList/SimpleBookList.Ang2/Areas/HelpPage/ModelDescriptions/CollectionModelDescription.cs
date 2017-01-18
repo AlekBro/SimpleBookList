a79f86e934f435c011aa8dff0712d641d95c4ce9
brova.aleksandr <brova.aleksandr@apriorit.com>
2017-01-18 17:41:17 +0200

    Added Ang2 project - copied Web API functionality.

---
+++ SimpleBookList/SimpleBookList.Ang2/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace SimpleBookList.Ang2.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ SimpleBookList/SimpleBookList.Ang2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace SimpleBookList.Ang2.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
